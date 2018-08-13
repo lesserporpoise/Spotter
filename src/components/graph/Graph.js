ce8fc0380f8fd92bafb831106a7204b7dd073798
Steven Penfield <stevenpenfield9@gmail.com>
2018-08-13 13:55:13 -0600

    graph works

--- src/components/graph/Graph.js
+++ src/components/graph/Graph.js
@@ -6,32 +6,42 @@ class Graph extends Component{
     constructor(props){
         super(props)
         this.state={
-            points:[],
             graphData:{
-                labels:['50yds','100yds','150yds','200yds','250yds','300yds',
+                labels:['0yds','50yds','100yds','150yds','200yds','250yds','300yds',
                 '350yds','400yds','450yds','500yds','550yds','600yds','650yds',
                 '700yds','750yds','800yds','850yds','900yds','950yds','1000yds'],
                 datasets:[
                     {
-                        label:['Bullet Drop'],
+                        label:['Vacuum Flight'],
                         data:[],
                         backgroundColor:['rgba(255,0,0)']
+                    },
+                    {
+                        label:['True Flight'],
+                        data:[],
+                        backgroundColor:['rgba(0,0,255)']
                     }
                 ]
             }
         }
     }
 
-computeData(props){
+computeData(){
     let distance = [0.1,50,100,150,200,250,300,350,400,450,500,550,600,650,700,750,800,850,900,950,1000]
-    let velocity = props.cardData.vm
+    let velocity = this.props.vm
     let splits = []
     let gFps = 32.1740
 
     splits = distance.map((x)=>{return x*3/velocity})
-    let gravSplits = splits.map((x)=>{return(.5*gFps*Math.pow(x,2))})
-    this.setState({points:gravSplits})
-    console.log(gravSplits)
+    let gravSplits = splits.map((x)=>{return(-1*(.5*gFps*Math.pow(x,2)))})
+    let newState={...this.state}
+    newState.graphData.datasets[0].data=gravSplits;
+    newState.graphData.datasets[1].data=gravSplits;
+    this.setState(newState);
+
+
+    console.log(this.state.points)
+    console.log(this.state.graphData.datasets[0].data)
 
 }
 
@@ -42,7 +52,7 @@ computeData(props){
   render(){
       return(
         <div className="graphBox">
-        <button onClick={()=>{this.computeData()}}></button>
+        <button onClick={()=>{this.computeData()}}>Calculate</button>
         <br/>
         <Line
             data={this.state.graphData}
@@ -51,9 +61,9 @@ computeData(props){
                 scales:{
                     yAxes:[{
                         ticks:{
-                            max:40,
-                            min:-80,
-                            stepSize:10
+                            max:10,
+                            min:-50,
+                            stepSize:5
                         }
                     }]
                 }
@@ -68,7 +78,7 @@ computeData(props){
 
 function mapStateToProps(state){
     return{
-        cardData:state.cardData
+        vm:state.cardData.vm
     }
 }
 
